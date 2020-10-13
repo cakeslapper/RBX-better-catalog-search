749afedf24f3fc53c4bac081314b467188981f7b
Danielle H <60787559+smallscripter@users.noreply.github.com>
2020-10-13 14:04:18 -0400

    a few optimization changes (more in desc)
    - removed the require jquery since i didn't use it at all
    - converted all promises in async functions to use await
    - added a try-catch-finally block in main(). hopefully it prevents the script from stopping in case of errors
    - changed "let" to "const" for unchanging variables
    - changed the catalogIDs array to form using a for loop instead of .map()

--- tampermonkey_script.js
+++ tampermonkey_script.js
@@ -4,52 +4,51 @@
 // @description  Remove the duplicate shirts and pants in the roblox catalog
 // @author       Danielle
 // @match        https://api.roblox.com/*
-// @require      https://code.jquery.com/jquery-3.5.1.js
 // @require      https://bundle.run/pixelmatch
 // ==/UserScript==
 
 /* global pixelmatch */
 const perfectThreshold = .5;
 
-let category = "&Category=3&Subcategory=12"
-let keywordSearch = "20015";
+const category = "&Category=3&Subcategory=12"
+const keywordSearch = "20021";
 
-let uniqueClothes = [];
+const uniqueClothes = [];
 
 let nextPageCursor = "";
-let baseApiUrl = "https://catalog.roblox.com/v1/search/items/details?Keyword=" + keywordSearch + category + "&cursor=";
+const baseApiUrl = "https://catalog.roblox.com/v1/search/items/details?Keyword=" + keywordSearch + category + "&cursor=";
 let usedApiUrl = baseApiUrl + nextPageCursor;
 
 function getLink(text) {
-    let linkIDstartIndex = text.indexOf("tr.rbxcdn.com/") + 14;
-    let linkIDendIndex = text.indexOf("/", linkIDstartIndex + 1);
-    let linkID = text.slice(linkIDstartIndex, linkIDendIndex);
-    let clothingType = "Shirt";
-    let link = "https://tr.rbxcdn.com/" + linkID + "/420/420/" + clothingType + "/Png";
+    const linkIDstartIndex = text.indexOf("tr.rbxcdn.com/") + 14;
+    const linkIDendIndex = text.indexOf("/", linkIDstartIndex + 1);
+    const linkID = text.slice(linkIDstartIndex, linkIDendIndex);
+    const clothingType = "Shirt";
+    const link = "https://tr.rbxcdn.com/" + linkID + "/420/420/" + clothingType + "/Png";
     return link;
 }
 
 function createImg(thumbnailLink) {
-    let img = new Image();
+    const img = new Image();
     img.src = thumbnailLink;
     img.crossOrigin = "Anonymous";
     return img;
 }
 
 function getImgData(img) {
-    let canvas = document.createElement('canvas');
+    const canvas = document.createElement('canvas');
     canvas.width = img.width;
     canvas.height = img.height
-    let ctx = canvas.getContext('2d');
+    const ctx = canvas.getContext('2d');
     ctx.drawImage(img, 0, 0);
-    let data = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
+    const data = ctx.getImageData(0, 0, canvas.width, canvas.height).data;
     return data;
 }
 
 function appendImg(img) {
     img.style.maxWidth = '150px';
     img.style.maxHeight = '150px';
-    let place = document.getElementById("Assets");
+    const place = document.getElementById("Assets");
     place.appendChild(img);
 }
 
@@ -60,7 +59,7 @@ function bothEqualImgData(a, b) {
 }
 
 function addIfUnique(eachClothing) {
-    let duplicate = uniqueClothes.some((uniqueClothing) => bothEqualImgData(eachClothing, uniqueClothing));
+    const duplicate = uniqueClothes.some((uniqueClothing) => bothEqualImgData(eachClothing, uniqueClothing));
     if (duplicate == false) {
         uniqueClothes.push(eachClothing);
     }
@@ -68,11 +67,11 @@ function addIfUnique(eachClothing) {
 
 function addToUniqueClothes(thumbnailLink, catalogLink) {
     if (!thumbnailLink.includes("html")) {
-        let img = createImg(thumbnailLink);
+        const img = createImg(thumbnailLink);
         img.onload = function() {
-            let data = getImgData(img);
+            const data = getImgData(img);
 
-            let eachClothing = {
+            const eachClothing = {
                 "catalogLink": catalogLink,
                 "imageData": data
             }
@@ -84,35 +83,40 @@ function addToUniqueClothes(thumbnailLink, catalogLink) {
 async function addEachClothing(catalogIDs) {
     let catalogIDlength = catalogIDs.length;
     for (let i = 0; i < catalogIDlength; ++i) {
-        let catalogID = catalogIDs[i];
+        const catalogID = catalogIDs[i];
 
-        let catalogLink = "https://www.roblox.com/catalog/" + catalogID;
-        fetch(catalogLink)
-            .then((response) => response.text())
-            .then((text) => {
-                let thumbnailLink = getLink(text);
-                addToUniqueClothes(thumbnailLink, catalogLink);
-            });
+        const catalogLink = "https://www.roblox.com/catalog/" + catalogID;
+        const catalogItemResponse = await fetch(catalogLink);
+        const catalogItemText = await catalogItemResponse.text();
+        const thumbnailLink = getLink(catalogItemText);
+        addToUniqueClothes(thumbnailLink, catalogLink);
     }
 }
 
 async function main() {
-    fetch(usedApiUrl)
-        .then((apiResponse) => apiResponse.json())
-        .then((apiJSON) => {
-            nextPageCursor = apiJSON.nextPageCursor;
-            usedApiUrl = baseApiUrl + nextPageCursor;
-            let catalogIDs = apiJSON.data.map(clothing => clothing.id);
-            addEachClothing(catalogIDs);
-            console.log("finished a page");
+    try {
+        const apiResponse = await fetch(usedApiUrl)
+        const apiJSON = await apiResponse.json()
 
-            if (nextPageCursor != null) {
-                main();
-            } else {
-                console.log("unique clothes: ");
-                console.log(uniqueClothes);
-            }
-        });
+        nextPageCursor = apiJSON.nextPageCursor;
+        usedApiUrl = baseApiUrl + nextPageCursor;
+        const catalogIDs = [];
+        const JSONdata = apiJSON.data;
+        for (let i=0; i<JSONdata.length; ++i) {
+            catalogIDs.push(JSONdata[i].id);
+        }
+        addEachClothing(catalogIDs);
+        console.log("finished a page");
+    } catch(error) {
+        throw 500;
+    } finally {
+        if (nextPageCursor != null) {
+            main();
+        } else {
+            console.log("unique clothes: ");
+            console.log(uniqueClothes);
+        }
+    }
 }
 
 main();
